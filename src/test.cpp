ae32fd5820f63bf6c0f6bf7e6134efeb3b27806e
gonervirt <gonertvirt@users.noreply.github.com>
2025-03-11 20:18:59 +0100

    2eme ajout

---
+++ src/test.cpp
@@ -0,0 +1,10 @@
+
+void test() {
+  // put your test code here, to run once:
+  // int result = myFunction(2, 3);
+  //if (result == 5) {
+    // Serial.println("Test passed!");
+  //} else {
+    // Serial.println("Test failed!");
+  // }
+}   
